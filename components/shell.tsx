0ceb32b40213ee7585d220ca69930eeac9d86da4
applywiseai <hire.ridoy@gmail.com>
2026-08-17 10:26:32 +0600

    Refactor Shell component to remove user auth
    Removed user authentication and sidebar from Shell component.

--- components/shell.tsx
+++ components/shell.tsx
@@ -1,22 +1,7 @@
-import { requireUser } from "@/lib/auth";
-import Sidebar from "@/components/sidebar";
-
-export default async function Shell({
+export default function Shell({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const user = await requireUser();
-
-  return (
-    <div>
-      <Sidebar user={user} />
-
-      <main className="md:ml-64 min-h-screen p-5 md:p-8 pt-16 md:pt-8">
-        <div className="max-w-7xl mx-auto">
-          {children}
-        </div>
-      </main>
-    </div>
-  );
+  return <>{children}</>;
 }
